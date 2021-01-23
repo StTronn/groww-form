7db4cec91e50c426c3fa1ef9892fe0f1f3fe900e
sttronn <rishav.21m@gmail.com>
2021-01-24 04:17:43 +0530

    added temp Text title

--- src/components/DetailCointainer/index.js
+++ src/components/DetailCointainer/index.js
@@ -4,7 +4,13 @@ import "./index.css";
 const DetailCointainer = () => {
   return <div className="cointainerDetails">
 
-    <div className="leftCointainerDetails"></div>
+    <div className="leftCointainerDetails">
+      
+				<div className="titleDetails">
+					  Bank 
+					<span style={{ color: "var(--primaryClr)" }}> Details</span>
+				</div>
+    </div>
     {/* this is only for animation */}
     <div className="imgBackgroundDetails"></div>
     </div>;

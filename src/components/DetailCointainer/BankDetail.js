536e774f888e6bbf5470ccefb8616141b9b97c7b
sttronn <rishav.21m@gmail.com>
2021-01-25 03:26:44 +0530

    UI done

---
+++ src/components/DetailCointainer/BankDetail.js
@@ -0,0 +1,51 @@
+import React from "react";
+import InputField from "../../styles/InputField";
+import PrimaryButton from "../../styles/PrimaryButton";
+import "./index.css";
+
+const BankDetail = () => {
+  return (
+    <div style={{ justifySelf: "center" }} className="fadeInUpDetails">
+      <div className="titleDetails">
+        Bank
+        <span style={{ color: "var(--primaryClr)" }}> Details</span>
+      </div>
+
+      <div className="fieldsDetails">
+        <InputField
+          id={"pan"}
+          name="pan"
+          placeholder={"Pan Card No"}
+          type="text"
+        />
+        <InputField
+          placeholder={"Bank Account No"}
+          name="bNo"
+          id={"bNo"}
+          type="text"
+        />
+        <InputField
+          placeholder={"IFSC Code"}
+          name="ifsc"
+          id={"ifsc"}
+          type="text"
+        />
+        <InputField
+          placeholder={"Bank Name"}
+          name="bName"
+          id={"bName"}
+          type="text"
+        />
+      </div>
+      <div className="centerButton">
+        <PrimaryButton style={{ width: "180px", height: "50px" }}>
+          Next
+        </PrimaryButton>
+      </div>
+      {/* this is only for animation */}
+      <div className="imgBackgroundDetails"></div>
+    </div>
+  );
+};
+
+export default BankDetail;
